ab1dde6168e1455e85cb7680657ac4f41c75334a
rajulapatisairam <rajulapatisairam@gmail.com>
2015-11-19 11:59:32 +0530

    Stateless configuration is done is added.

--- src/com/practice/hibernate/configuration/SessionUtils.java
+++ src/com/practice/hibernate/configuration/SessionUtils.java
@@ -1,16 +1,23 @@
 package com.practice.hibernate.configuration;
 
 import org.hibernate.Session;
+import org.hibernate.StatelessSession;
 
 public class SessionUtils implements AutoCloseable{
        Session session;
+       StatelessSession statelessSession;
 
     public SessionUtils() {
         this.session = HibernateUtil.getSession();
+        this.statelessSession = HibernateUtil.getStateLessSession();
 	}
 
 	public Session getSession() {
 		return session;
+	}
+	
+	public StatelessSession getStatelessSession() {
+		return statelessSession;
 	}
 
 	@Override
@@ -19,6 +26,9 @@ public class SessionUtils implements AutoCloseable{
 	        if(this.session != null){
 	        	this.session.clear();
 	        	this.session.close();
+	        }
+	        if( this.statelessSession !=null){
+	        	this.statelessSession.close();
 	        }
 	}
 }
